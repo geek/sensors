80f742c4d1fb9137214b553532a5f22741f20589
Wyatt Preul <wpreul@gmail.com>
2014-10-29 15:42:09 -0500

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,22 @@
+// Load modules
+
+var Code = require('code');
+var Lab = require('lab');
+
+
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var expect = Code.expect;
+
+
+describe('Sensors', function () {
+
+});
