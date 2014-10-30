765aa59102f5fbc03699e97da1ffdd937b262587
Wyatt Preul <wpreul@gmail.com>
2014-10-30 12:59:37 -0500

    Basics for parse

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 var Code = require('code');
 var Lab = require('lab');
+var Sensors = require('../');
 
 
 
@@ -14,9 +15,19 @@ var internals = {};
 
 var lab = exports.lab = Lab.script();
 var describe = lab.describe;
+var it = lab.it;
 var expect = Code.expect;
 
 
 describe('Sensors', function () {
 
+    describe('parse()', function () {
+
+        it('parses a single line', function (done) {
+
+            var result = Sensors.parse('12;6;0;0;3;1.4\n');
+            expect(result[0].subType).to.equal('S_LIGHT');
+            done();
+        });
+    });
 });
